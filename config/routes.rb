e900bd73c08e308476170f0cdb3d124f37a7494a
Steve Kenworthy <steveyken@gmail.com>
2013-03-28 11:43:56 +0800

    Locked down routes (for security) and added more tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  match "/vend/customer_update" => "vend#customer_update", :format => :json
-  match "/vend/register_sale" => "vend#register_sale", :format => :json
+  post "/vend/customer_update" => "vend#customer_update", :format => :json
+  post "/vend/register_sale"   => "vend#register_sale",   :format => :json
 
-  match "/vend/customer_update_mq" => "vend#customer_update_mq", :format => :json
-  match "/vend/register_sale_mq" => "vend#register_sale_mq", :format => :json
+  post "/vend/customer_update_mq" => "vend#customer_update_mq", :format => :json
+  post "/vend/register_sale_mq"   => "vend#register_sale_mq",   :format => :json
 
-  match "/admin/ffcrm_vend" => "admin/ffcrm_vend#index",  :via => :get,  :format => :html
-  match "/admin/ffcrm_vend" => "admin/ffcrm_vend#update", :via => :post, :format => :html
+  get "/admin/ffcrm_vend" => "admin/ffcrm_vend#index",  :format => :html
+  put "/admin/ffcrm_vend" => "admin/ffcrm_vend#update", :format => :html
 
 end
--- spec/routing/vend_spec.rb
+++ spec/routing/vend_spec.rb
@@ -2,8 +2,51 @@ require 'spec_helper'
 
 describe VendController do
   describe "routing" do
-    it "generates register_sale route for vend" do
-      { :get => "/vend/register_sale" }.should route_to(:controller => "vend", :action => "register_sale", :format => :json)
+
+    describe "register_sale" do
+      it "POST json" do
+        { :post => "/vend/register_sale" }.should route_to(:controller => "vend", :action => "register_sale", :format => :json)
+      end
+      it "GET json" do
+        { :get => "/vend/register_sale" }.should_not route_to(:controller => "vend", :action => "register_sale", :format => :json)
+      end
     end
+
+    describe "register_sale_mq" do
+      it "POST json" do
+        { :post => "/vend/register_sale_mq" }.should route_to(:controller => "vend", :action => "register_sale_mq", :format => :json)
+      end
+      it "GET json" do
+        { :get => "/vend/register_sale_mq" }.should_not route_to(:controller => "vend", :action => "register_sale_mq", :format => :json)
+      end
+    end
+
+    describe "customer_update" do
+      it "POST json" do
+        { :post => "/vend/customer_update" }.should route_to(:controller => "vend", :action => "customer_update", :format => :json)
+      end
+      it "GET json" do
+        { :get => "/vend/customer_update" }.should_not route_to(:controller => "vend", :action => "customer_update", :format => :json)
+      end
+    end
+
+    describe "customer_update_mq" do
+      it "POST json" do
+        { :post => "/vend/customer_update_mq" }.should route_to(:controller => "vend", :action => "customer_update_mq", :format => :json)
+      end
+      it "GET json" do
+        { :get => "/vend/customer_update_mq" }.should_not route_to(:controller => "vend", :action => "customer_update_mq", :format => :json)
+      end
+    end
+
+    describe "admin/vend" do
+      it "GET html" do
+        { :get => "/admin/ffcrm_vend" }.should route_to(:controller => "admin/ffcrm_vend", :action => "index", :format => :html)
+      end
+      it "PUT html" do
+        { :put => "/admin/ffcrm_vend" }.should route_to(:controller => "admin/ffcrm_vend", :action => "update", :format => :html)
+      end
+    end
+
   end
 end
