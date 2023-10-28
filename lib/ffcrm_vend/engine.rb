8a6ec7af412f2964534be272c78eafafb3200972
Stephen Kenworthy <steveyken@gmail.com>
2023-10-28 15:49:10 +0800

    Engine setup tweaks

--- lib/ffcrm_vend/engine.rb
+++ lib/ffcrm_vend/engine.rb
@@ -4,14 +4,14 @@ module FfcrmVend
     config.to_prepare do
       tab_urls = FatFreeCRM::Tabs.admin.map{|tab| tab[:url]}.map{|url| url[:controller]}
       unless tab_urls.include? 'admin/ffcrm_vend'
-        FatFreeCRM::Tabs.admin << {:url => { :controller => "admin/ffcrm_vend" }, :text => "Vend", :icon => 'fa-usd'}
+        FatFreeCRM::Tabs.admin << {url: { controller: "admin/ffcrm_vend" }, text: "Vend", icon: 'fa-usd'}
       end
     end
 
     # Tell Rails that when generating models, controllers for this engine to use RSpec and FactoryBot, instead of the default of Test::Unit and fixtures
     config.generators do |g|
-      g.test_framework      :rspec,        :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.test_framework      :rspec,       fixture: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
       g.assets false
       g.helper false
     end
